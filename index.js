0867fb37f5b01666547eafce57ffc4aa3c2d9676
itsLyLee <lyylee00@gmail.com>
2020-10-24 15:24:46 +0200

    update

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const app = express();
 //Import Routes
 const authRoute = require("./routes/auth");
 
+//Route Middlewares
+app.use("/api/user", authRoute);
+
 app.listen(3000, () => console.log("Server Up and running"));
