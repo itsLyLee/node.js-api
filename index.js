37a11ed0eebac3f3e58646b52a4d2f38f61bc9df
itsLyLee <lyylee00@gmail.com>
2020-10-24 15:21:46 +0200

    update

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+const { Router } = require("express");
 const express = require("express");
 const app = express();
+
+router.post("/register", (req, res) => {
+  res.send("Register");
+});
 
 app.listen(3000, () => console.log("Server Up and running"));
