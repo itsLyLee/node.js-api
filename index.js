691c8118f2a4cda4a12f7abf0d502093d60fd319
itsLyLee <lyylee00@gmail.com>
2020-10-26 20:05:21 +0100

    update

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express();
 const mongoose = require("mongoose");
 
 //Connect to DB
-mongoose.connect();
+mongoose.connect(
+  "mongodb+srv://LyLee:Draconis9301@cluster0.qkpvq.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  () => console.log("connected to db!")
+);
 
 //Import Routes
 const authRoute = require("./routes/auth");
