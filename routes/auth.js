0cf922c7220d9e5e1355912b9c7f830a6544a464
itsLyLee <lyylee00@gmail.com>
2020-10-27 15:50:44 +0100

    hapi/joi

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const User = require("../model/User");
 
+//VALIDATION
+const Joi = require("@hapi/joi");
+
 router.post("/register", async (req, res) => {
   const user = new User({
     name: req.body.name,
