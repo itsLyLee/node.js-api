924a5c534823cb881788387b9379c2a638a81caf
itsLyLee <lyylee00@gmail.com>
2020-10-27 15:41:54 +0100

    update async

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,7 @@ router.post("/register", async (req, res) => {
   });
   try {
     const savedUser = await user.save();
+    res.send(savedUser);
   } catch (err) {
     res.status(400).send(err);
   }
