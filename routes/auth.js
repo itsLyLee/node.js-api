8000d61616eae8e6cc53aa56c3a17c227b437f0c
itsLyLee <lyylee00@gmail.com>
2020-10-29 22:11:42 +0100

    @hapi/joi update API

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,8 @@ const Joi = require("@hapi/joi");
 
 const schema = Joi.object({
   name: Joi.string().min(6).required(),
-  email: Joi.string().min(6).required().email,
-  password: Joi.string().min(6).required,
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required(),
 });
 
 router.post("/register", async (req, res) => {
