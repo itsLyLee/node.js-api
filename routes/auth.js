fa53ead82d7abb322bd6d3881001752c8f0f0e69
itsLyLee <lyylee00@gmail.com>
2020-10-29 21:41:18 +0100

    @hapi/joi update

--- routes/auth.js
+++ routes/auth.js
@@ -4,15 +4,15 @@ const User = require("../model/User");
 //VALIDATION
 const Joi = require("@hapi/joi");
 
-const schema = {
+const schema = Joi.object({
   name: Joi.string().min(6).required(),
   email: Joi.string().min(6).required().email,
   password: Joi.string().min(6).required,
-};
+});
 
 router.post("/register", async (req, res) => {
   //VALIDATE THE DATA BEFORE WE MAKE A USER
-  const { error } = Joi.validate(req.body, schema);
+  const { error } = schema.validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const user = new User({
