3403a4d7e7ab9e0a0f8a9be80aeff9ff2bec39a3
itsLyLee <lyylee00@gmail.com>
2020-10-30 23:35:41 +0100

    validation.js update schema

--- routes/validation.js
+++ routes/validation.js
@@ -1,8 +1,11 @@
 //VALIDATION
 const Joi = require("@hapi/joi");
 
-const schema = Joi.object({
-  name: Joi.string().min(6).required(),
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required(),
-});
+//Register Validation
+const registerValidation = () => {
+  const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  }
+};
