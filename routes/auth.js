4ee0d6e1ef624693eae4f8496141141f0b0bdcd8
itsLyLee <lyylee00@gmail.com>
2020-10-31 00:18:39 +0100

    bcrypt hash

--- routes/auth.js
+++ routes/auth.js
@@ -14,6 +14,7 @@ router.post("/register", async (req, res) => {
 
   //Hash passwords
   const salt = await bcrypt.gentSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
 
   //Create a new user
   const user = new User({
