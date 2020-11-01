7f3190e424e32c9849e04a01396c09c358d8fd21
itsLyLee <lyylee00@gmail.com>
2020-11-01 18:53:50 +0100

    update password

--- routes/auth.js
+++ routes/auth.js
@@ -9,18 +9,18 @@ router.post("/register", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   //Check if the user is already in the database
-  const emailExist = await User.findOne({ email: req.body.email });
-  if (emailExist) return res.status(400).send("Email already exists");
+  const emailExist = await User.findOne({email: req.body.email});
+  if(emailExist) return res.status(400).send("Email already exists");
 
   //Hash passwords
-  const salt = await bcrypt.gentSalt(10);
+  const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
   //Create a new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.hashedpassword,
+    password: hashedpassword,
   });
   try {
     const savedUser = await user.save();
