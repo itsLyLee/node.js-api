73799254860176788970146a0ccee67b2b927f10
itsLyLee <lyylee00@gmail.com>
2020-11-04 23:57:33 +0100

    jsonwebtoken function update verify

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -3,4 +3,8 @@ const jwt = require("jsonwebtoken");
 function auth(req, res, next) {
   const token = req.header("auth-token");
   if (!token) return res.status(401).send("Access Denied");
+
+  try {
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+  } catch (err) {}
 }
