42f0df68ca9e5a5cf57e992855e714f2f79ff39b
itsLyLee <lyylee00@gmail.com>
2020-11-05 23:40:00 +0100

    posts.js update

--- routes/posts.js
+++ routes/posts.js
@@ -1,1 +1,10 @@
 const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    posts: {
+      title: "my first post",
+      description: "random data you shouldnt access",
+    },
+  });
+});
