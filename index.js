6b956c59a5e3be7bd13a8ef4c6c2aa5d58afcec1
itsLyLee <lyylee00@gmail.com>
2020-11-07 20:05:52 +0100

    posts verifyToken update

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ const postRoute = require("./routes/posts");
 dotenv.config();
 
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
-  console.log("connected to db!")
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("connected to db!")
 );
 
 //Middleware
